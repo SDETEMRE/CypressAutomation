4118ff939fe5772609f9839fe63748ef38843728
Emre <91288077+SDETEMRE@users.noreply.github.com>
2024-03-05 12:41:26 +0300

    addded some test cases

--- cypress/e2e/udemy_lessons/contactForm.cy.js
+++ cypress/e2e/udemy_lessons/contactForm.cy.js
@@ -1,22 +1,30 @@
 ///<reference types="cypress" />
+require("@cypress/xpath");
 
 describe("Contact form", () => {
+  before(function () {
+    cy.fixture("userDetails").as("user");
+  });
   it("submit form", () => {
     cy.visit("https://www.automationteststore.com/");
-    cy.title();
-    cy.get("a[href$='contact']")
+
+    cy.get("a[href$='contact']") //$ means ends with in CSS locator ^ starts with and * contains
       .click()
-      .then("textOftheContact", function (linkText) {
+      .then(function (linkText) {
         cy.log("clicked on link using text: " + linkText.text());
-        console.log(linkText.text());
       });
-  });
-  it("origin usage", () => {
-    cy.origin("www.google.com", () => {
-      cy.visit("/");
+    cy.get("@user").then((user) => {
+      //cy.get("#ContactUsFrm_first_name").type(user.first_name);
+      cy.xpath("//input[@name = 'first_name']").type(user.first_name);
+      cy.get("#ContactUsFrm_email").type(user.email);
     });
-    cy.origin("www.amazon.com", () => {
-      cy.visit("/");
-    });
+
+    cy.get("#ContactUsFrm_email").should("have.attr", "name", "email");
+    cy.get("#ContactUsFrm_enquiry").type("Do you provide...");
+    cy.get("button[title='Submit']").click();
+    cy.get(".mb40 > :nth-child(3)").should(
+      "have.text",
+      "Your enquiry has been successfully sent to the store owner!"
+    );
   });
 });
